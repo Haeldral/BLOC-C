d717e2f208d326b834ac80aa0bf8bc7b3f248bf2
Romain Felgines <romain.felgines@gmail.com>
2021-11-20 22:35:31 -0500

    plus qu'un bug

--- src/dynamique/VoitureFactory.java
+++ src/dynamique/VoitureFactory.java
@@ -2,11 +2,6 @@ package dynamique;
 
 import voiture.Voiture;
 import voiture.VoitureSport;
-import java.lang.reflect.*;
-import dynamique.ByteArrayClass;
-import dynamique.ByteArrayClasseLoader;
-import dynamique.Test;
-import dynamique.TestFactory;
 
 import javax.tools.*;
 import javax.tools.JavaFileObject;
@@ -18,38 +13,24 @@ import java.util.List;
 
 
 public class VoitureFactory {
-	
+
 	public enum ModeConstruction {INSTANCIATION, REFLEXION, META}
-	
+
 	public static Voiture buildVoiture(ModeConstruction mode, boolean sport, int vitesse) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException {
-		
+
 		Voiture voiture;
 		Class classe;
-		
+
+
 		if (sport) {
 			switch(mode){
 				case META:
-					
-					preparationfactory();
-					
-					StringBuilder sb = new StringBuilder();
-
-			        sb.append("package voiture;\n");
-			        sb.append("public class " + nomClasse + " extends Voiture implements Surveillable{\n");
-			        genererAttributMetaVoitureSport(sb);
-			        genererConstructeurMetaVoitureSport(nomClasse, x, sb);
-			        genererMethodeMetaVoitureSport(sb);
-			        sb.append("}\n");
-
-			        System.out.println("LA CLASSE");
-			        System.out.println(sb.toString());
-
-			        return new StringSource(nomClasse, sb.toString());
+					voiture = preparationFactory("MetaVoitureSport");
 					break;
 				case REFLEXION:
 					classe = Class.forName("voiture.VoitureSport");
 					voiture = (Voiture)classe.getDeclaredConstructor().newInstance();
-					break;	
+					break;
 				default:
 					voiture = new VoitureSport();
 					break;
@@ -57,19 +38,7 @@ public class VoitureFactory {
 		} else {
 			switch(mode){
 			case META:
-				StringBuilder sb = new StringBuilder();
-
-		        sb.append("package voiture;\n");
-		        sb.append("public class " + nomClasse + " extends Voiture implements Surveillable{\n");
-		        genererAttributMetaVoiture(sb);
-		        genererConstructeurMetaVoiture(nomClasse, x, sb);
-		        genererMethodeMetaVoiture(sb);
-		        sb.append("}\n");
-
-		        System.out.println("LA CLASSE");
-		        System.out.println(sb.toString());
-
-		        return new StringSource(nomClasse, sb.toString());
+				voiture = preparationFactory("MetaVoiture");
 				break;
 			case REFLEXION:
 				classe = Class.forName("voiture.Voiture");
@@ -82,10 +51,26 @@ public class VoitureFactory {
 		}
 		return voiture;
 	}
-	
+
+	private static StringSource buildString(String nomClasse) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("package voiture;\n");
+		sb.append("public class " + nomClasse + " extends " + nomClasse.replace("Meta","") + "implements Surveillable{\n");
+		genererAttributMetaVoitureSport(sb);
+		genererConstructeurMetaVoitureSport(nomClasse, sb);
+		genererMethodeMetaVoitureSport(sb);
+		sb.append("}\n");
+
+		System.out.println("LA CLASSE");
+		System.out.println(sb.toString());
+
+		return new StringSource(nomClasse, sb.toString());
+	}
+
 	// FACTORY POUR METAVOITURE
 
-    private static void genererConstructeurMetaVoiture(String nomClasse, int x, StringBuilder sb) {
+    private static void genererConstructeurMetaVoiture(String nomClasse, StringBuilder sb) {
 
         sb.append("public " + nomClasse + "(int vitesse) {\r\nsuper(vitesse);\r\n}\n");
     }
@@ -101,11 +86,11 @@ public class VoitureFactory {
         sb.append("private int vitesse = 50;\n");
     }
 
-    
+
  // FACTORY POUR METAVOITURESPORT
 
-	
-    private static void genererConstructeurMetaVoitureSport(String nomClasse, int x, StringBuilder sb) {
+
+    private static void genererConstructeurMetaVoitureSport(String nomClasse, StringBuilder sb) {
 
         sb.append("public " + nomClasse + "() {\r\nsuper();\r\n}\n");
     }
@@ -120,18 +105,18 @@ public class VoitureFactory {
 
         sb.append("private int vitesse = 50;\n");
     }
-    
-    
-    
-    
-    
-    void preparationFactory(){
+
+
+
+
+
+    static Voiture preparationFactory(String nomClasse){
 	 // ******** ETAPE #1 : Préparation pour la compilation
 	    JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-	    List<ByteArrayClass> classes = new ArrayList<>();           // pour mettre les .class   (IMPORTANT)
+	    List<ByteArrayClass> classes = new ArrayList<ByteArrayClass>();           // pour mettre les .class   (IMPORTANT)
 	    DiagnosticCollector<JavaFileObject> collector = new DiagnosticCollector<JavaFileObject>();
 	    JavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
-	
+
 	    // La classe qui se charge de fournir les "conteneurs" au compilateur à la volée, sans accès au disque
 	    fileManager = new ForwardingJavaFileManager<JavaFileManager>(fileManager){
 	        public JavaFileObject getJavaFileForOutput(Location location, String className, JavaFileObject.Kind kind,
@@ -145,40 +130,35 @@ public class VoitureFactory {
 	                return super.getJavaFileForOutput(location, className, kind, sibling);
 	        }
 	    };
-	
+
 	    // ******** ETAPE #2 : Génération du code source
-	    List<JavaFileObject> sources = List.of(
-	            TestFactory.buildSource("MetaVoiture"),
-	            TestFactory.buildSource("MetaVoitureSport")
-	    );
-	
+	    List<JavaFileObject> sources = List.of(buildString(nomClasse));
+
 	    // ******** ETAPE #3 : Compilation
 	    JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, collector, null,
 	            null, sources);
 	    Boolean result = task.call();
-	
+
 	    for (Diagnostic<? extends JavaFileObject> d : collector.getDiagnostics())
 	        System.out.println(d);
-	
+
 	    try {
 	        fileManager.close();
 	    } catch (IOException e) {
 	        e.printStackTrace();
 	    }
-	
+
 	    if (!result) {
 	        System.out.println("ECHEC DE LA COMPILATION");
 	        System.exit(1);
 	    }
-	
-	
+
+
 	    // ******** ETAPE #4 : Instanciation
 	    ByteArrayClasseLoader loader = new ByteArrayClasseLoader(classes);
-	    List<Test> mesTests = new ArrayList<Test>();
 	    try {
 	        // Recherche la classe dans le contexte "local" sinon il passe par le "loader"
-	        mesTests.add((Test)(Class.forName("dynamique.MetaVoiture", true, loader).getDeclaredConstructor().newInstance()));
-	        mesTests.add((Test)(Class.forName("dynamique.MetaVoitureSport", true, loader).getDeclaredConstructor().newInstance()));
+	        return (Voiture)(Class.forName("dynamique."+ nomClasse, true, loader).getDeclaredConstructor().newInstance());
 	    } catch (ClassNotFoundException | NoSuchMethodException e) {
 	        e.printStackTrace();
 	    } catch (IllegalAccessException e) {
@@ -188,12 +168,7 @@ public class VoitureFactory {
 	    } catch (InvocationTargetException e) {
 	        e.printStackTrace();
 	    }
-	
-	    // ******** ETAPE #5 : Exécution
-	    for (Test t : mesTests){
-	        System.out.println("CLASSE : " + t.getClass());
-	        System.out.println("X : " + t.getX());
-	    }
+		System.out.println("La creation meta a échouée");
+		return new Voiture(1);
 	}
-}
 }
